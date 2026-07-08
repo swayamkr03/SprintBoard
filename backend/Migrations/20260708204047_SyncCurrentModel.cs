e38115b228f64ee728723f319e31f841739bd970
Swayam Kumar <Swayamkumar312@gmail.com>
2026-07-09 02:11:13 +0530

    Add migration for current EF model

---
+++ backend/Migrations/20260708204047_SyncCurrentModel.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SprintBoard.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncCurrentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectIssues_projects_ProjectId",
+                table: "ProjectIssues");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_projects",
+                table: "projects");
+
+            migrationBuilder.RenameTable(
+                name: "projects",
+                newName: "Projects");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Projects",
+                table: "Projects",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectIssues_Projects_ProjectId",
+                table: "ProjectIssues",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectIssues_Projects_ProjectId",
+                table: "ProjectIssues");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Projects",
+                table: "Projects");
+
+            migrationBuilder.RenameTable(
+                name: "Projects",
+                newName: "projects");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_projects",
+                table: "projects",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectIssues_projects_ProjectId",
+                table: "ProjectIssues",
+                column: "ProjectId",
+                principalTable: "projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
